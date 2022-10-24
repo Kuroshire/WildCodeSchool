f8ef03ec0079148f6c3497a589201c5e44af39d9
Kuroshire <hugo.bounichou@gmail.com>
2022-10-24 19:32:39 +0200

    i wanna die pls someone shoot me

--- entry_test_wcs/index.cjs
+++ entry_test_wcs/index.cjs
@@ -24,9 +24,9 @@ app.use(express.urlencoded());
 app.use("/api/auth", authRoute);
 
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, '.')));
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '.', 'index.html'));
 });
 
 mongoose
